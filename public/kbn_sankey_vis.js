c708eed38e7d9fcb8413bb56906dfb987a9d0206
Itay Weinberger <itay@joo.la>
2018-01-29 00:20:19 +0200

    align with version 6

--- public/kbn_sankey_vis.js
+++ public/kbn_sankey_vis.js
@@ -1,7 +1,15 @@
-import { CATEGORY } from 'ui/vis/vis_category';
-import { VisFactoryProvider } from 'ui/vis/vis_factory';
-import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
-import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import {
+  CATEGORY
+} from 'ui/vis/vis_category';
+import {
+  VisFactoryProvider
+} from 'ui/vis/vis_factory';
+import {
+  VisSchemasProvider
+} from 'ui/vis/editors/default/schemas';
+import {
+  VisTypesRegistryProvider
+} from 'ui/registry/vis_types';
 import kbnSankeyVisTemplate from 'plugins/kbn_sankey_vis/kbn_sankey_vis.html';
 
 import 'ui/agg_table';
@@ -22,34 +30,37 @@ function KbnSankeyVisProvider(Private) {
     icon: 'fa-random',
     description: 'Sankey charts are ideal for displaying the material, energy and cost flows.',
     category: CATEGORY.DATA,
-    template: kbnSankeyVisTemplate,
-    params: {
+    visConfig: {
+      template: kbnSankeyVisTemplate,
       defaults: {
         showMetricsAtAllLevels: false
-      },
-      editor: '<vislib-basic-options></vislib-basic-options>'
+      }
     },
     hierarchicalData: function (vis) {
       return Boolean(vis.params.showPartialRows || vis.params.showMetricsAtAllLevels);
     },
-    schemas: new Schemas([{
-      group: 'metrics',
-      name: 'metric',
-      title: 'Split Size',
-      min: 1,
-      max: 1,
-      defaults: [{
-        type: 'count',
-        schema: 'metric'
-      }]
-    }, {
-      group: 'buckets',
-      name: 'segment',
-      title: 'Split Slices',
-      aggFilter: '!geohash_grid',
-      min: 0,
-      max: Infinity
-    }]),
-    requiresSearch: true
+    editorConfig: {
+      optionsTemplate: '<vislib-basic-options></vislib-basic-options>',
+      schemas: new Schemas([{
+        group: 'metrics',
+        name: 'metric',
+        title: 'Split Size',
+        min: 1,
+        max: 1,
+        defaults: [{
+          type: 'count',
+          schema: 'metric'
+        }]
+      }, {
+        group: 'buckets',
+        name: 'segment',
+        title: 'Split Slices',
+        aggFilter: '!geohash_grid',
+        min: 0,
+        max: Infinity
+      }]),
+    },
+    requiresSearch: true,
+    responseHandler: 'none'
   });
 }
